0d555ddf3ee64dd93d7c6bd08bceeeaf89b8a64c
palashjhanwar10 <70604674+palashjhanwar10@users.noreply.github.com>
2020-10-02 02:11:47 +0530

    Update ExampleInstrumentedTest.java

--- WeatherTest/app/src/androidTest/java/com/example/vamshi/weathertest/ExampleInstrumentedTest.java
+++ WeatherTest/app/src/androidTest/java/com/example/vamshi/weathertest/ExampleInstrumentedTest.java
@@ -19,8 +19,8 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        Context app_Context = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.vamshi.weathertest", appContext.getPackageName());
+        assertEquals("com.example.vamshi.weathertest", app_Context.getPackageName());
     }
 }
